de4758cead40d55d7ca7275600dfdf288369a364
moreliabeltran <21130049@e-itesca.edu.mx>
2025-01-28 16:48:05 -0700

    PRACTICA3.0

---
+++ app/src/main/java/mx/edu/itesca/practica3/MainActivity.kt
@@ -0,0 +1,33 @@
+package mx.edu.itesca.practica3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_get_started)
+
+        button.setOnClickListener {
+            var intent: Intent=Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/mx/edu/itesca/practica3/RegisterActivity.kt
@@ -0,0 +1,28 @@
+package mx.edu.itesca.practica3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        val button: Button = findViewById(R.id.btn_sign_in)
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this,MenuActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
